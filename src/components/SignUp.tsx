71325a0c73e0c30a68ac55504f8d3a8516e37c39
Colton <czpemberton@gmail.com>
2021-02-15 17:58:55 -0800

    feat: sign up page

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,14 +1,35 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
+import { useAuth } from '../context/AuthContext'
 
 export const SignUp = () => {
   const emailRef = useRef<HTMLInputElement>(null)
   const passwordRef = useRef<HTMLInputElement>(null)
   const passwordConfirmRef = useRef<HTMLInputElement>(null)
+  const { signUp, user } = useAuth()
+  const [error, setError] = useState<string>('')
+  const [loading, setLoading] = useState<boolean>(false)
+
+  async function handleSubmit(e: any) {
+    // ! fix any
+    e.preventDefault()
+    if (passwordRef.current!.value !== passwordConfirmRef.current!.value) {
+      return setError('Passwords do not match')
+    }
+    try {
+      setError('')
+      setLoading(true)
+      await signUp(emailRef.current!.value, passwordRef.current!.value)
+    } catch {
+      setError('Attempt to sign up failed')
+    }
+    setLoading(false)
+  }
 
   return (
     <div className="sign-up card">
       <h2>Sign Up</h2>
-      <form className="sign-up-form" action="">
+      {error && <span className="error">{error}</span>}
+      <form className="sign-up-form" onSubmit={handleSubmit}>
         <label htmlFor="email">Email:</label>
         <input id="email" type="email" ref={emailRef} required />
 
@@ -18,7 +39,7 @@ export const SignUp = () => {
         <label htmlFor="password-confirm">Confirm Password:</label>
         <input id="password-confirm" type="password" ref={passwordConfirmRef} required />
 
-        <input type="submit" />
+        <input disabled={loading} type="submit" />
       </form>
       <p>
         Have an account? <a href="">Sign in</a>
